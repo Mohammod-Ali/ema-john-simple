a58666b31e3fcaf1c9d8aca1901a25ac28261090
Mohammod Ali <mohammodali651@gmail.com>
2022-09-28 02:10:52 +0600

    show data from local storage

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,7 +7,7 @@ import "./Shop.css";
 const Shop = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
-  console.log(cart);
+
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
@@ -42,10 +42,19 @@ const Shop = () => {
     setCart(savedCart)
   }, [products]);
 
-  const handleAddToCart = (product) => {
-    const newCart = [...cart, product];
+  const handleAddToCart = (selectedProduct) => {
+    let newCart = [];
+    const exists = cart.find(product => product.id === selectedProduct.id)
+    if(!exists){
+        selectedProduct.quantity = 1;
+        newCart = [...cart, selectedProduct]
+    }else{
+        const rest = cart.filter(product => product.id !== selectedProduct.id)
+        exists.quantity = exists.quantity + 1;
+        newCart = [...rest, exists]
+    }
     setCart(newCart);
-    addToDb(product.id);
+    addToDb(selectedProduct.id);
   };
 
   return (
