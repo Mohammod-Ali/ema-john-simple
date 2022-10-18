2a7bf256b9cae8c2f5681e52d05be3589669ee15
Mohammod Ali <mohammodali651@gmail.com>
2022-10-18 14:24:46 +0600

    create sign up page and usercontext

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SignUp.css'
 
 const SignUp = () => {
     return (
-        <div>
-            this is sign up
+        <div className='form-container'>
+            <h1 className='form-title'>Sign Up</h1>
+            <form >
+                <div className='form-control'>
+                    <label htmlFor='email'>Email</label>
+                    <input type="email" name="email" required/>
+                </div>
+                <div className='form-control'>
+                    <label htmlFor='password'>Password</label>
+                    <input type="password" name="password" required/>
+                </div>
+                <div className='form-control'>
+                    <label htmlFor='confirm'> Confirm Password</label>
+                    <input type="password" name="confirm" required/>
+                </div>
+                <input className='btn-submit' type="submit" value="Sign Up" />
+            </form>
+            <p>Already have an Account<Link to='/Login'>Login</Link></p>
         </div>
     );
 };
